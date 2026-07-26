4c0536eaa31589e43b0f3ce79861b457f858fe50
kentamitsui <kentam12@gmail.com>
2026-07-26 22:07:48 +0900

    - コードを修正   - 関数の分割

--- 練習問題/1-5/問題2：線形探索（名前探し）.c
+++ 練習問題/1-5/問題2：線形探索（名前探し）.c
@@ -1,22 +1,27 @@
-# include <stdio.h>
-# include <stdbool.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-int main(void) {
-  int target = 3;
-  int checkItems1[] = {1,2,3,4,5};
-  int checkItems2[] = {1,2,3,4};
-  bool result = false;
-
-  int length1 = sizeof(checkItems1) / sizeof(checkItems1[0]);
-  int length2 = sizeof(checkItems2) / sizeof(checkItems2[0]);
-
-  for (int i = 0; i < length1; i++) {
-    if (checkItems1[i] == target) {
-      result = true;
+bool list_search(const int items[], int length, int target) {
+  for (int i = 0; i < length; i++) {
+    if (items[i] == target) {
+      return true;
       break;
     }
   }
+  return false;
+}
 
-  printf("%s\n", result ? "true" : "false");
+int main(void) {
+  int target = 3;
+  int checkItems1[] = {1, 2, 3, 4, 5};
+  int checkItems2[] = {1, 2, 4, 5};
+
+  int itemsLength1 = sizeof(checkItems1) / sizeof(checkItems1[0]);
+  int itemsLength2 = sizeof(checkItems2) / sizeof(checkItems2[0]);
+
+  printf("%s\n",
+         list_search(checkItems1, itemsLength1, target) ? "true" : "false");
+  printf("%s\n",
+         list_search(checkItems2, itemsLength2, target) ? "true" : "false");
   return 0;
 }
