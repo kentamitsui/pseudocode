a1ff605087db07c720de3d089a721206f19fa28e
kentamitsui <kentam12@gmail.com>
2026-07-28 00:19:11 +0900

    - Pythonコードを記述

---
+++ 練習問題/1-5/問題3：垂直に積み上がるレンガ（マリオ問題）.py
@@ -0,0 +1,12 @@
+
+
+def stackBricks(height: int) -> list[str]:
+    result: list[str] = []
+
+    while height > 0:
+        result.append("#\n")
+        height -= 1
+    return result
+
+
+print("".join(stackBricks(5)), end="")
