38551e26d3f3d0fd18e65e8b3748679f7beeb237
kentamitsui <kentam12@gmail.com>
2026-07-28 01:58:09 +0900

    - Cのコード追加 - .gitignoreの更新

---
+++ 練習問題/1-5/問題3：垂直に積み上がるレンガ（マリオ問題）.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  int height = 5;
+  char result[64] = ""; // あらかじめ最大63文字 + 終端のヌル文字\0で埋める
+
+  while (height > 0) {
+    strcat(result, "#\n"); // strcatは既存の文字列の後ろに連結する役割がある
+    height -= 1;
+  }
+
+  printf("%s", result); // リストの先頭から\0の手前まで出力
+  return 0;
+}
