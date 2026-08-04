1c36f3eed66e795e4c215d627a643ad6c62ca894
kentamitsui <kentam12@gmail.com>
2026-08-04 09:30:50 +0900

    - Cの改善コードを追加

--- 練習問題/1-5/問題4：リスト内の最大値を見つける.c
+++ 練習問題/1-5/問題4：リスト内の最大値を見つける.c
@@ -9,12 +9,24 @@ int search_targetValue(const int array[], int result, int len) {
   return result;
 }
 
+// 改善版
+int find_max(const int array[], int len) {
+  int max_value = array[0];
+  for (int i = 1; i < len; i++) {
+    if (array[i] > max_value) {
+      max_value = array[i];
+    }
+  }
+  return max_value;
+}
+
 int main(void) {
   int searchList[] = {3, 4, 9, 5, 13, -1, 0, 7};
   int result = searchList[0];
   int length = sizeof(searchList) / sizeof(searchList[0]);
 
   printf("%d\n", search_targetValue(searchList, result, length));
+  printf("%d\n", find_max(searchList, length));
 
   return 0;
 }
