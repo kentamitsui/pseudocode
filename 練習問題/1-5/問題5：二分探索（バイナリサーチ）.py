d4823307b24b7031320a59dbc0e2bcecc15ca76a
kentamitsui <kentam12@gmail.com>
2026-08-10 23:53:17 +0900

    - Pythonのコードを追加

---
+++ 練習問題/1-5/問題5：二分探索（バイナリサーチ）.py
@@ -0,0 +1,38 @@
+import random
+import math
+
+
+def binary_search(arr: list[int], target: int) -> bool:
+    low: int = 0
+    high: int = len(arr) - 1
+
+    while low <= high:
+        mid: int = math.floor((low + high) / 2)
+        if (target == arr[mid]):
+            return True
+        elif (target < arr[mid]):
+            high = mid - 1
+        else:
+            low = mid + 1
+    return False
+
+
+def random_target() -> int:
+    return random.randint(0, 100)
+
+
+def random_arr(n: int) -> list[int]:
+    return sorted(random.sample(range(5, 101), n))
+
+
+def main() -> None:
+    arr = random_arr(random_target())
+    target = random_target()
+    found = binary_search(arr, target)
+
+    print(f"target: {target}")
+    print(f"searchArr: {arr}")
+    print(found)
+
+
+main()
